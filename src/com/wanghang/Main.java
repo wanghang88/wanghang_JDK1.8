d0630da7adad620586130022e779353ecd687ab4
wanghang88 <wanghang712@qq.com>
2021-04-26 08:02:33 +0800

    Initial commit

---
+++ src/com/wanghang/Main.java
@@ -0,0 +1,18 @@
+package com.wanghang;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Map<String,Object> map=new HashMap<>();
+
+        map.put("a",1);
+        map.put("b",2);
+        map.put("c",3);
+
+        System.out.println(map);
+    }
+}
