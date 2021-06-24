baea865fdfa28eb8db418dc302110d8ac722a16f
wanghang <wb-wx772876@alibaba-inc.com>
2021-06-24 11:01:51 +0800

    update

--- src/wanghang/jvm/JVMDemo.java
+++ src/wanghang/jvm/JVMDemo.java
@@ -4,7 +4,24 @@ package wanghang.jvm;
 /**
  *jvm的相关知识：
   1：java的内存模型；
- https://mp.weixin.qq.com/s?__biz=MzAwMDg2OTAxNg==&mid=2652048697&idx=1&sn=82cd7cf5ab2ab9a34a4ca1e6e246f46e&scene=21#wechat_redirect
+  https://mp.weixin.qq.com/s?__biz=MzAwMDg2OTAxNg==&mid=2652048697&idx=1&sn=82cd7cf5ab2ab9a34a4ca1e6e246f46e&scene=21#wechat_redirect
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ 2:JVM的常见面试总结:
+   https://blog.csdn.net/v123411739/article/details/79671697
+
+   JVM的内存区域，分代模型，垃圾回收算法，触发GC:
+   https://mp.weixin.qq.com/s/FZxHVdli_EKmrZ8qviDy8g
+
+   java文件怎么加载到JVM的:
+   https://mp.weixin.qq.com/s?__biz=Mzg3MjA5OTkzMw==&mid=2247483723&idx=1&sn=4d0e6ec7f962946bf454b95452a6dc13&chksm=cef53879f982b16f552b77b0877807282edac46123372a1c4e46911f377ca3013a190b6ebe16&scene=178&cur_album_id=1711135604001800193#rd
+ *
  *
  *
  */
