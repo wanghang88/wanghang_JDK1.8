78b294717923922b7f91febb088be2ca439a0b00
wanghang <wb-wx772876@alibaba-inc.com>
2021-06-29 10:53:24 +0800

    update

--- src/wanghang/juc/aqs/AbstractQueuedSynchronizer.java
+++ src/wanghang/juc/aqs/AbstractQueuedSynchronizer.java
@@ -2,12 +2,13 @@ package wanghang.juc.aqs;
 
 
 /**
- *aqs:AbstractQueuedSynchronizer同步队列，它是实现同步器的基础组件
- * 参考博文：
+  aqs:AbstractQueuedSynchronizer同步队列，它是实现同步器的基础组件
+  参考博文：
   https://mp.weixin.qq.com/s?__biz=MzAwMDg2OTAxNg==&mid=2652048892&idx=1&sn=1b12dc819ec677a2af67875d7fbbe4a0&scene=21#wechat_redirect
- *
- *
- *
+
+
+
+
  */
 public class AbstractQueuedSynchronizer {
 }
