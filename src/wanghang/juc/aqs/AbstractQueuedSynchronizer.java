8f6413631145ec8ff7f0b815df52c803da7edd10
wanghang <wb-wx772876@alibaba-inc.com>
2021-06-30 17:03:43 +0800

    update

--- src/wanghang/juc/aqs/AbstractQueuedSynchronizer.java
+++ src/wanghang/juc/aqs/AbstractQueuedSynchronizer.java
@@ -2,16 +2,23 @@ package wanghang.juc.aqs;
 
 
 /**
-  aqs:AbstractQueuedSynchronizer同步队列，它是实现同步器的基础组件
+  1:aqs:AbstractQueuedSynchronizer同步队列，它是实现同步器的基础组件
   参考博文：
   https://mp.weixin.qq.com/s?__biz=MzAwMDg2OTAxNg==&mid=2652048892&idx=1&sn=1b12dc819ec677a2af67875d7fbbe4a0&scene=21#wechat_redirect
 
 
 
 
-  AQS原理分析的博文讲解得不错
+  2:AQS原理分析的博文讲解得不错
   https://www.cnblogs.com/waterystone/p/4920797.html
   https://www.cnblogs.com/chengxiao/archive/2017/07/24/7141160.html#a1-1
+  AQS原理分析也不错：
+  https://www.jianshu.com/p/b7ec536c9ed7
+
+
+
+  3:同步工具类Semaphore,CountDownLatch,CyclicBarrier原理以及用用的深入分析
+  https://www.jianshu.com/p/4556f0f3b9cb
 
 
 
