cf29b044460bd46b628f52719845e07c6e6e2f1b
wanghang <wb-wx772876@alibaba-inc.com>
2021-06-30 11:47:03 +0800

    update

--- src/wanghang/lock/SynchronizedDemo.java
+++ src/wanghang/lock/SynchronizedDemo.java
@@ -2,12 +2,12 @@ package wanghang.lock;
 
 
 /**
- *1:深入理解Synchronized
+ 1:深入理解Synchronized
  https://mp.weixin.qq.com/s?__biz=MzAwMDg2OTAxNg==&mid=2652048775&idx=1&sn=fa4cca84abe9aa9c58ebb926f10141e4&scene=21#wechat_redirect
- *
- *
- *
- *
+
+ 2:Java 并发中的锁优化和线程池优化
+ https://mp.weixin.qq.com/s?__biz=MzU0OTk3ODQ3Ng==&mid=2247505680&idx=1&sn=0d5cef430a9ad85565739641ef00f753&chksm=fba53f13ccd2b6052c6622b559b224b0f4d34cf60748781a37b6e0cb057a72984b9cf2668bde&scene=132#wechat_redirect
+
  */
 public class SynchronizedDemo {
 }
