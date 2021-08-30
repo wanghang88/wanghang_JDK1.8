7be4feb303d2171534a698a598eb3b80a93bdc52
wanghang <wb-wx772876@alibaba-inc.com>
2021-08-30 19:35:34 +0800

    update

--- src/wanghang/java/JavaDemo.java
+++ src/wanghang/java/JavaDemo.java
@@ -10,8 +10,13 @@ package wanghang.java;
 
 
  2:java本地调试环境的搭建,这个两个差不多，可以结合一期看
- https://blog.csdn.net/qq_26020387/article/details/105126951?utm_medium=distribute.pc_relevant.none-task-blog-baidujs_title-0&spm=1001.2101.3001.4242
  https://blog.csdn.net/yiyihuazi/article/details/111661769
+
+ 其中最重要的两点:
+ 分步：创建普通的java工程,将原来JDK解压下的Src导入
+ 1:设置中配置让debug模式
+ 2:SOURCEPATH设置成你目录中的src目录，原来的删除掉，更换成解压出来的Src的目录。
+   其中javafx-src是JAVA SE界面库，如果没有使用到可以不加载源代码
  */
 public class JavaDemo {
 }
