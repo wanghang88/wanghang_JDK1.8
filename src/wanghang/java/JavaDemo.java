28e6fa438454bfda3faf830356e4a2ee3d5853e7
wanghang <wb-wx772876@alibaba-inc.com>
2021-08-30 19:19:03 +0800

    update

--- src/wanghang/java/JavaDemo.java
+++ src/wanghang/java/JavaDemo.java
@@ -9,8 +9,9 @@ package wanghang.java;
 
 
 
- 2:java本地调试环境的搭建
+ 2:java本地调试环境的搭建,这个两个差不多，可以结合一期看
  https://blog.csdn.net/qq_26020387/article/details/105126951?utm_medium=distribute.pc_relevant.none-task-blog-baidujs_title-0&spm=1001.2101.3001.4242
+ https://blog.csdn.net/yiyihuazi/article/details/111661769
  */
 public class JavaDemo {
 }
