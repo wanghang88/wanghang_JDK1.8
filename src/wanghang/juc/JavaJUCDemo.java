c88b70b94aef74490c2dea245e95bdca1400c180
wanghang <wb-wx772876@alibaba-inc.com>
2021-08-31 11:12:35 +0800

    update

--- src/wanghang/juc/JavaJUCDemo.java
+++ src/wanghang/juc/JavaJUCDemo.java
@@ -3,7 +3,7 @@ package wanghang.juc;
 /**
 
 
- 公众号程序员阿星的juc专题
+ 公众号程序员阿星的juc专题,对于Synchronized,AbstractQueuedSynchronizer(同步器),LockSupport,CAS,ThreadLocal,JMM(java的内存模型)的总结可以看看
  https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzAwMDg2OTAxNg==&action=getalbum&album_id=1826996971702665222&scene=173&from_msgid=2652049208&from_itemidx=1&count=3&nolastread=1#wechat_redirect
 
 
