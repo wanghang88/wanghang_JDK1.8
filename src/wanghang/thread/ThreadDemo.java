90ee01e5d885abf58d65bfd7a2be20cee8c63911
wanghang <wb-wx772876@alibaba-inc.com>
2021-08-31 11:25:13 +0800

    update

--- src/wanghang/thread/ThreadDemo.java
+++ src/wanghang/thread/ThreadDemo.java
@@ -3,11 +3,13 @@ package wanghang.thread;
 
 /**
  *线程相关的基础知识：
- *1:线程的状态：
- https://mp.weixin.qq.com/s?__biz=MzAwMDg2OTAxNg==&mid=2652050137&idx=1&sn=3ce2d77b6185812f94ab8d957ca9936b&chksm=8105e52eb6726c38da162f39e804144156031d4711a98b52bf4edacf58436c3af4e11cce2d0b&scene=178&cur_album_id=1772933467471691779#rd
- *
- *
- *
+   1:线程的状态：
+   https://mp.weixin.qq.com/s?__biz=MzAwMDg2OTAxNg==&mid=2652050137&idx=1&sn=3ce2d77b6185812f94ab8d957ca9936b&chksm=8105e52eb6726c38da162f39e804144156031d4711a98b52bf4edacf58436c3af4e11cce2d0b&scene=178&cur_album_id=1772933467471691779#rd
+
+
+   2:Java线程间通信之wait/notify机制
+   https://www.cnblogs.com/chengxiao/p/7043529.html
+
  */
 public class ThreadDemo {
 
