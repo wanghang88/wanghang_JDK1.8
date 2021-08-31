ca208676bf7698329a1e87efa759459f1faf6c5a
wanghang <wb-wx772876@alibaba-inc.com>
2021-08-31 11:30:33 +0800

    update

--- src/wanghang/jvm/JVMDemo.java
+++ src/wanghang/jvm/JVMDemo.java
@@ -5,17 +5,9 @@ package wanghang.jvm;
  *jvm的相关知识：
   1：java的内存模型；
   https://mp.weixin.qq.com/s?__biz=MzAwMDg2OTAxNg==&mid=2652048697&idx=1&sn=82cd7cf5ab2ab9a34a4ca1e6e246f46e&scene=21#wechat_redirect
- *
- *
- *
- *
- *
- *
- *
- *
+
  2:JVM的常见面试总结:
    https://blog.csdn.net/v123411739/article/details/79671697
-
 
    java文件怎么加载到JVM的:
    https://mp.weixin.qq.com/s?__biz=Mzg3MjA5OTkzMw==&mid=2247483723&idx=1&sn=4d0e6ec7f962946bf454b95452a6dc13&chksm=cef53879f982b16f552b77b0877807282edac46123372a1c4e46911f377ca3013a190b6ebe16&scene=178&cur_album_id=1711135604001800193#rd
@@ -29,25 +21,15 @@ package wanghang.jvm;
    java内存分策略与GC算法:
    https://www.jianshu.com/p/0c7cd3811f7b
 
-
    JVM的垃圾搜集器:
    https://www.jianshu.com/p/dcbb42ab9f4c
-
 
    JVM GC运行原理(主要是垃圾回收原理以及怎么排查FullGC)
    https://mp.weixin.qq.com/s/P8s3kuceBNovUP5adXpFCQ
 
-
-
-
-
    整个JVM总结得比较全的:
    https://www.jianshu.com/nb/29039435
 
-
- *
- *
- *
  */
 public class JVMDemo {
 
---
+++ src/wanghang/thread/ThreadPoolDemo.java
@@ -0,0 +1,20 @@
+package wanghang.thread;
+
+
+/**
+
+ 线程池：的几个关键问题介绍;
+ 核心参数,
+ 工作原理,
+ 监控,
+ 调优手段
+ https://mp.weixin.qq.com/s/5LPyBFAHJklRrL2WPWJQxg
+
+
+
+
+
+
+ */
+public class ThreadPoolDemo {
+}
