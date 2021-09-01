19a3c9645ada05cefb708feadb3ea7afc32f8618
wanghang <wb-wx772876@alibaba-inc.com>
2021-09-01 12:06:37 +0800

    update

---
+++ src/wanghang/java/JavaDemo.java
@@ -0,0 +1,22 @@
+package wanghang.java;
+
+
+/**
+ *
+ vovo关于使用java语言的总结（
+ 1:关于多线程上下文传递
+ 2：JMM和volatile
+ 3：java8的Stream原理
+ 4：字节码原理及实战
+ 5：java的spi机制
+ ）
+ https://mp.weixin.qq.com/mp/appmsgalbum?action=getalbum&album_id=1500551436353503236&__biz=MzI4NjY4MTU5Nw==#wechat_redirect
+
+
+
+
+
+
+ */
+public class JavaDemo {
+}
