3c63255587bd6291e43eb0e9bd4c280858ab93f9
wanghang <wb-wx772876@alibaba-inc.com>
2021-09-22 16:53:08 +0800

    update

--- src/wanghang/algorithm/AlgorithmDemo.java
+++ src/wanghang/algorithm/AlgorithmDemo.java
@@ -1,10 +1,14 @@
 package wanghang.algorithm;
 
 /**
-
-
  算法：
  https://mp.weixin.qq.com/mp/appmsgalbum?__biz=Mzg5NDY3NzIwMA==&action=getalbum&album_id=2042935422749589505#wechat_redirect
+
+ 常见的排序算法:
+ http://javainterview.gitee.io/luffy/2021/09/01/11-%E7%AE%97%E6%B3%95/01.%20%E6%8E%92%E5%BA%8F%E7%AE%97%E6%B3%95/
+
+ LeetCode Easy级别的算法:
+ http://javainterview.gitee.io/luffy/2021/09/02/11-%E7%AE%97%E6%B3%95/02.%20LeetCode%20Easy/
 
  */
 public class AlgorithmDemo {
